579b7cad8d68fa372774343dcebc6edde305189a
kodumurnaseer-001 <kodumurnaseer@gmail.com>
2026-03-10 01:39:30 +0530

    Add SOS blinking pattern to Arduino sketch
    Implement blinking pattern for Morse code SOS signal using pin 13.

---
+++ Lesson_02_SOS(MORSE_CODE).ino
@@ -0,0 +1,49 @@
+int pin13=13;
+int dt1=50;
+int dt2=500;
+int dt=1000;
+void setup(){
+  pinMode(pin13,OUTPUT);
+}
+void loop(){
+  digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt1);
+digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt1);
+digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt1);
+digitalWrite(pin13,HIGH);
+delay(dt2);
+digitalWrite(pin13,LOW);
+delay(dt2);
+digitalWrite(pin13,HIGH);
+delay(dt2);
+digitalWrite(pin13,LOW);
+delay(dt2);
+digitalWrite(pin13,HIGH);
+delay(dt2);
+digitalWrite(pin13,LOW);
+delay(dt2);
+digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt1);
+digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt1);
+digitalWrite(pin13,HIGH);
+delay(dt1);
+digitalWrite(pin13,LOW);
+delay(dt);
+}
+
+
+
+
