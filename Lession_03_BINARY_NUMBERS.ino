f5f0918eb3eceb4d3b1632b59da0ee8b6c830913
kodumurnaseer-001 <kodumurnaseer@gmail.com>
2026-03-10 02:03:59 +0530

    Add binary number display logic using LEDs

---
+++ Lession_03_BINARY_NUMBERS.ino
@@ -0,0 +1,116 @@
+int pin13=11;
+int pin12=10;
+int pin11=8;
+int pin10=4;
+int dt=1000;
+void setup(){
+  pinMode(pin13,OUTPUT);
+  pinMode(pin12,OUTPUT);
+  pinMode(pin11,OUTPUT);
+  pinMode(pin10,OUTPUT);
+
+
+}
+void loop(){
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,LOW);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,LOW);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,LOW);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,LOW);
+  delay(dt);
+  digitalWrite(pin13,HIGH);
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,HIGH);
+  digitalWrite(pin10,HIGH);
+  delay(dt);
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+}
